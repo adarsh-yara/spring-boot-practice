f78431bc44e6b708fcb6622358eaacd09740951c
Adarsh Kumar <95460787+adarsh-yara@users.noreply.github.com>
2023-05-13 13:30:21 +0530

    add TestController

---
+++ src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> getTestMessage() {
+        return new ResponseEntity<>("Test Message", HttpStatus.OK);
+    }
+}
